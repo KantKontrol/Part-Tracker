80ebccfaccf7a720ee123d5e15b7c6fe1673c4da
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-25 16:33:06 -0400

    created start of choosepart page

--- front-end/partrac-standalone/src/App.js
+++ front-end/partrac-standalone/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ChooseModel from "./pages/ChooseModel";
+import ChoosePart from "./pages/ChoosePart";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <ChooseModel />
+      <ChoosePart />
       <Footer />
     </div>
   );
---
+++ front-end/partrac-standalone/src/pages/ChoosePart.js
@@ -0,0 +1,15 @@
+import React from "react";
+import VersionStamp from "../components/VersionStamp";
+
+export default function ChoosePart(){
+
+
+    return (
+        <div>
+            <VersionStamp />
+
+
+            
+        </div>
+    );
+}

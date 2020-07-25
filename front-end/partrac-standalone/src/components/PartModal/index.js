e1d58027a20b24a734bbbf6e6bbc3a598ca57bb8
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-25 17:53:21 -0400

    added input box for quantity in part modal and changed 'agree' to 'submit'

--- front-end/partrac-standalone/src/components/PartModal/index.js
+++ front-end/partrac-standalone/src/components/PartModal/index.js
@@ -10,10 +10,17 @@ export default function PartModal(props){
             <div className="modal-content">
                 <img className="modal-image" src="https://via.placeholder.com/150" alt="..."></img>
                 <h4>{title + " - " + model}</h4>
-                <p>Enter the quantity</p>
+                <p>Enter the quantity...</p>
+                <div class="col s4">
+                    <div class="input-field inline">
+                        <input id="email_inline" type="email" class="validate" />
+                        <label for="email_inline">Quantity</label>
+                    </div>
+                    pc/pcs.
+                </div>
             </div>
             <div className="modal-footer">
-                <a href="#!" className="modal-close waves-effect waves-green btn-flat">Agree</a>
+                <a href="#!" className="modal-close waves-effect waves-green btn-flat">Submit</a>
             </div>
         </div>
     );

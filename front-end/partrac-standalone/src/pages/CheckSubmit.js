45993f48e2a0ac93349428e045fda068ab1eae39
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-25 18:16:12 -0400

    created base for check and submit page

---
+++ front-end/partrac-standalone/src/pages/CheckSubmit.js
@@ -0,0 +1,15 @@
+import React from "react";
+import VersionStamp from "../components/VersionStamp";
+
+export default function CheckSubmit(){
+
+
+    return (
+        <div>
+            <VersionStamp />
+            <div className="container">
+
+            </div>
+        </div>
+    );
+}

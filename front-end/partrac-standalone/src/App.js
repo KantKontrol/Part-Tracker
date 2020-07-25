4e046480b7a1e120cf364d0a98e81867bde6964f
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-25 18:23:56 -0400

    added title and button to CheckSubmit pafe

--- front-end/partrac-standalone/src/App.js
+++ front-end/partrac-standalone/src/App.js
@@ -3,12 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 import ChooseModel from "./pages/ChooseModel";
 import ChoosePart from "./pages/ChoosePart";
+import CheckSubmit from "./pages/CheckSubmit";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div>
-      <ChoosePart />
+      <CheckSubmit />
       <Footer />
     </div>
   );
--- front-end/partrac-standalone/src/pages/CheckSubmit.js
+++ front-end/partrac-standalone/src/pages/CheckSubmit.js
@@ -7,8 +7,17 @@ export default function CheckSubmit(){
     return (
         <div>
             <VersionStamp />
+
+            <h1>Verify your items...</h1>
             <div className="container">
 
+
+                <div className="row">
+                    <div className="col s2">
+                        <button>Submit items</button>
+                    </div>
+                    <div className="col s10"></div>
+                </div>
             </div>
         </div>
     );

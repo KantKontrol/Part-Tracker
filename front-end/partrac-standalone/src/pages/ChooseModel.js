9581863daca6e00f34d002d2488675c326b75b7f
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-25 16:08:30 -0400

    added materialize cdn

--- front-end/partrac-standalone/src/pages/ChooseModel.js
+++ front-end/partrac-standalone/src/pages/ChooseModel.js
@@ -8,7 +8,12 @@ export default function ChooseModel(){
     return (
         <div>
             <h1>Choose Model</h1>
-   
+
+            <div className="container" style={{ flex: "1 0 auto"}}>
+                <div className="row">
+  
+                </div>
+            </div>
         </div>
     );
 }

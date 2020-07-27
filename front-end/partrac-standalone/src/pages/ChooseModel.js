3b8367cf51548ef0067666b6df8753db9fb89c8f
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-27 16:22:36 -0400

    added check items and submit button to choose a model page

--- front-end/partrac-standalone/src/pages/ChooseModel.js
+++ front-end/partrac-standalone/src/pages/ChooseModel.js
@@ -1,6 +1,7 @@
 import React from "react";
 import VersionStamp from "../components/VersionStamp";
 import ModelSelectDisplay from "../components/ModelSelectDisplay";
+import { Link } from "react-router-dom";
 
 export default function ChooseModel(){
 
@@ -10,8 +11,16 @@ export default function ChooseModel(){
         <div>
             <VersionStamp />
             <h1 style={{ width: "max-content", margin: "20px auto"}}>Choose a Model...</h1>
+            
 
             <div className="container">
+                <div className="row">
+                    <div className="col s4"></div>
+                    <div className="col s4">
+                        <Link to="/checksubmit" className="btn" style={{ display: "block", fontSize: 1.4 + "rem" }}>Check Items and Submit</Link>
+                    </div>
+                    <div className="col s4"></div>
+                </div>
                 <div className="row">
                     <ModelSelectDisplay />
                     <ModelSelectDisplay />

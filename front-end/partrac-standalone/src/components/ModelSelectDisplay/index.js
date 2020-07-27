bc7123bc248a34be4536e9c3725da2fb5753c504
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-27 16:44:49 -0400

    made clickable models more visibly clickable

--- front-end/partrac-standalone/src/components/ModelSelectDisplay/index.js
+++ front-end/partrac-standalone/src/components/ModelSelectDisplay/index.js
@@ -7,7 +7,7 @@ export default function ModelSelectDisplay(){
 
     return (
         <div className="col s4">
-            <Link to="/choosepart"><div className="picture-cover" style={{ backgroundImage: "url(https://via.placeholder.com/250)"}}></div></Link>
+            <Link to="/choosepart"><div className="picture-cover hover-display" style={{ backgroundImage: "url(https://via.placeholder.com/250)"}}></div></Link>
         </div>
     );
 }

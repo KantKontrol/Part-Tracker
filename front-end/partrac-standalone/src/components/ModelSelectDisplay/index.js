6a5c7dad3307403b01b9757923b1892acb532432
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-27 15:21:42 -0400

    added link to choosemodel page to load part screen

--- front-end/partrac-standalone/src/components/ModelSelectDisplay/index.js
+++ front-end/partrac-standalone/src/components/ModelSelectDisplay/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./index.css";
+import { Link } from "react-router-dom";
 
 export default function ModelSelectDisplay(){
 
 
     return (
         <div className="col s4">
-            <div className="picture-cover" style={{ backgroundImage: "url(https://via.placeholder.com/250)"}} onClick={() => console.log("Clicked a model!")}></div>
+            <Link to="/choosepart"><div className="picture-cover" style={{ backgroundImage: "url(https://via.placeholder.com/250)"}}></div></Link>
         </div>
     );
 }

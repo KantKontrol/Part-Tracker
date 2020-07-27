1e51912b45f2f467849de1fcd99d35c6c11a57c6
KantKontrol <nick.derissio.tech@gmail.com>
2020-07-27 16:23:55 -0400

    added navigation back to models page from check submit

--- front-end/partrac-standalone/src/pages/CheckSubmit.js
+++ front-end/partrac-standalone/src/pages/CheckSubmit.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import VersionStamp from "../components/VersionStamp";
 
 export default function CheckSubmit(){
@@ -11,10 +12,17 @@ export default function CheckSubmit(){
             <h1>Verify your items...</h1>
             <div className="container">
 
+                <div className="row">
+                    <div className="col s4">
+                        <Link to="/" className="btn">{"<- Models"}</Link>
+                    </div>
+                    <div className="col s8"></div>
+                </div>
+
 
                 <div className="row">
                     <div className="col s2">
-                        <button>Submit items</button>
+                        <button >Submit items</button>
                     </div>
                     <div className="col s10"></div>
                 </div>

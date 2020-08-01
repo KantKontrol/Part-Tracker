12877160a7ef4d7c5508fd513bd1d31b706cb6b3
KantKontrol <nick.derissio.tech@gmail.com>
2020-08-01 16:25:19 -0400

    began adding sections to landing page

--- back-end/partrac-admin/client/src/pages/AdminLanding.js
+++ back-end/partrac-admin/client/src/pages/AdminLanding.js
@@ -9,7 +9,20 @@ export default function AdminLanding(){
             
 
             <div className="container">
-                
+                <div className="row">
+                    <div className="col s4">
+                        <div className="section">
+                            <h1>Test</h1>
+                        </div>
+                        <div className="divider"></div>
+                        <div className="section">
+                            <h1>Test 2</h1>
+                        </div>
+                    </div>
+                    <div className="col s8">
+
+                    </div>
+                </div>
             </div>
         </div>
     );

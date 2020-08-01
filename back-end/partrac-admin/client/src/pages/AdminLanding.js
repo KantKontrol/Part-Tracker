5286520be8f2e01bcbe54c9f570ea61952efe6a3
KantKontrol <nick.derissio.tech@gmail.com>
2020-08-01 16:16:14 -0400

    created boiler plate header component

--- back-end/partrac-admin/client/src/pages/AdminLanding.js
+++ back-end/partrac-admin/client/src/pages/AdminLanding.js
@@ -4,8 +4,12 @@ import React from "react";
 export default function AdminLanding(){
 
     return (
-        <div className="container">
+        <div>
+            
 
+            <div className="container">
+                
+            </div>
         </div>
     );
 }

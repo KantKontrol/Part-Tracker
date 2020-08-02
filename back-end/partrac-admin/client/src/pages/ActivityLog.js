9c7b9bd038ccfaa5ef538e9efe4d1893b07122ae
KantKontrol <nick.derissio.tech@gmail.com>
2020-08-02 17:23:24 -0400

    built out log page

--- back-end/partrac-admin/client/src/pages/ActivityLog.js
+++ back-end/partrac-admin/client/src/pages/ActivityLog.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Header from "../components/Header";
-
+import TableRow from "../components/TableRow";
 
 export default function ActivityLog(){
 
@@ -10,7 +11,37 @@ export default function ActivityLog(){
 
             <div className="container">
 
+                <h3 style={{ textAlign: "center" }}>Activity Log</h3>
 
+                <div className="row">
+                    <div className="col s4">
+                        <Link to="/home" className="btn">Back</Link>
+                    </div>
+                </div>
+
+
+                <div className="row">
+                    <div className="col s2"></div>
+
+                    <div className="col s8">
+                        <table className="striped">
+                            <thead>
+                                <tr>
+                                    <th>Initials</th>
+                                    <th>Parts Checked out</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <TableRow model="ND" part="4" />
+                                <TableRow model="ND" part="6" />
+                                <TableRow model="BS" part="13" />
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div className="col s2"></div>
+                </div>
             </div>
         </div>
     );

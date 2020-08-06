4268aa8aed5e6d13199ee19d81c278620a5f7e1a
KantKontrol <nick.derissio.tech@gmail.com>
2020-08-06 15:07:57 -0400

    modified layout and size of inputs

--- back-end/partrac-admin/client/src/components/ModelForm/index.js
+++ back-end/partrac-admin/client/src/components/ModelForm/index.js
@@ -19,7 +19,7 @@ export default function ModelForm(props){
         <div className="col s12 model-form-style">
 
             <div className="row">
-                <div className="input-field col s4">
+                <div className="input-field col s6 m4 l4">
                     <input id="model_name" type="text" className="validate" value={state.model_name} onChange={stateChange} />
                     <label htmlFor="model_name">Model Name</label>
                 </div>
@@ -28,23 +28,19 @@ export default function ModelForm(props){
             <h5 style={{ textAlign: "center" }}>Enter Part Quantity</h5>
 
             <div className="row">
-                <div className="input-field col s4">
+                <div className="input-field col s6 m4 l4">
                         <input id="keyboard_quantity" type="number" min="0" className="validate" value={state.keyboard_quantity} onChange={stateChange} />
                         <label htmlFor="keyboard_quantity"  className="active">Keyboard Quantity</label>
                 </div>
-                <div className="input-field col s4">
+                <div className="input-field col s6 m4 l4">
                         <input id="screen_quantity" type="number" min="0" className="validate" value={state.screen_quantity} onChange={stateChange}/>
                         <label htmlFor="screen_quantity"  className="active">Screen Quantity</label>
                 </div>
-                <div className="input-field col s4">
+                <div className="input-field col s6 m4 l4">
                         <input id="battery_quantity" type="number" min="0" className="validate" value={state.battery_quantity} onChange={stateChange}/>
                         <label htmlFor="battery_quantity"  className="active">Battery Quantity</label>
                 </div>
-            </div>
-
-            
-            <div className="row">
-                <div className="input-field col s4">
+                <div className="input-field col s6 m4 l4">
                         <input id="mobo_quantity" type="number" min="0" className="validate" value={state.mobo_quantity} onChange={stateChange}/>
                         <label htmlFor="mobo_quantity"  className="active">Mobo Quantity</label>
                 </div>
@@ -57,8 +53,6 @@ export default function ModelForm(props){
                     <button className="btn" style={{ float: "right" }} onClick={() => addModel()}>Add Model</button>
                 </div>
             </div>
-
-
 
         </div>
     );
